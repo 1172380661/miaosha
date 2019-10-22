52f39faabb6a4910ed3545db5c118c3ac7b3e97d
wsw <1172380661@qq.com>
2019-10-22 11:05:27 +0800

    去除不需要版本库管理的文件

--- src/main/java/my/miaosha/dao/MiaoshaDao.java
+++
@@ -1,9 +0,0 @@
-package my.miaosha.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MiaoshaDao {
-
-
-}
--- src/main/java/my/miaosha/redis/prefix/KeyPrefix.java
+++
@@ -1,9 +0,0 @@
-package my.miaosha.redis.prefix;
-
-public interface KeyPrefix {
-	
-	public int expireSeconds();
-	
-	public String getPrefix();
-	
-}
